6c890f5572bf2a94b2cf4334366ce2970d109688
Saksham Goyal <s.goyal@samsung.com>
2024-03-18 23:36:24 +0530

    set device for model load

--- runners/diffpose_frame.py
+++ runners/diffpose_frame.py
@@ -107,8 +107,8 @@ class Diffpose(object):
         # load pretrained model
         if model_path:
             logging.info('initialize model by:' + model_path)
-            states = torch.load(model_path)
-            self.model_pose.load_state_dict(states[0], map_location=self.device)
+            states = torch.load(model_path, map_location=self.device)
+            self.model_pose.load_state_dict(states[0])
         else:
             logging.info('initialize model randomly')
 
